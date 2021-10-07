74443fe3511bddbec7ce05d200e4f18b0380d3ce
cyan-ding <blueplus.d@gmail.com>
2021-10-06 20:06:34 -0700

    finished part 5

--- app/src/main/java/com/example/miniscout/MatchInputActivity.kt
+++ app/src/main/java/com/example/miniscout/MatchInputActivity.kt
@@ -1,11 +1,49 @@
 package com.example.miniscout
-
+import com.google.gson.Gson
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.match_input_activity.*
+import android.widget.Toast
+import android.content.Intent
+import android.util.Log
+import android.widget.EditText
+
+lateinit var intent:Intent
+public var match_tag = "Match"
 
 class MatchInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.match_input_activity)
     }
+
+    fun getTeamNumber(): String {
+        return et_team_number.text.toString()
+    }
+
+    fun getMatchNumber(): String {
+        return et_match_number.text.toString()
+    }
+
+    private fun isEditTextNotEmpty(edittext: List<EditText>): Boolean {
+        for (item in edittext) {
+            if (item.text.toString() == "") {
+                return false
+            }
+
+        }
+        return true
+    }
+
+    fun startNextActivity(view: View) {
+        intent = Intent(this,ScoutingActivity::class.java)
+        if (isEditTextNotEmpty(listOf(et_match_number,et_team_number))) {
+        intent.putExtra(match_tag, Gson().toJson(Match(getTeamNumber(), getMatchNumber())))
+            Log.e("match_data","${Gson().fromJson(intent.extras!!.get(match_tag).toString(), Match::class.java)}")
+        startActivity(Intent(this, ScoutingActivity::class.java))
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/miniscout/ScoutingActivity.kt
@@ -0,0 +1,8 @@
+package com.example.miniscout
+
+import android.app.Activity
+
+class ScoutingActivity: Activity(){
+
+}
+
